d738e60abbe32476e4d5443910a97def6d6831d5
Winston <winston.barlow@code.berlin>
2020-03-04 00:06:12 +0100

    error: 1, failure: 1

--- logic_test.py
+++ logic_test.py
@@ -1,17 +1,18 @@
 import unittest
 from logic import Calculator
+from math import isclose
 
 
 class TestLogic(unittest.TestCase):
 
     def test_monthly_repayments(self):
-        result = Calculator(15000, 70, 4.5, 15, 45100).monthly_repayments(self)
+        result = Calculator(150000, 70, 4.5, 15, 45100).monthly_repayments()
         self.assertEqual(result, 803.24, 144583.2, 39583.2, 180)
 
     def test_min_deposit(self):
-        result = Calculator(15000, 70, 4.5, 15,
-                            45100).calc_loan_mindeposit(self)
-        self.assertEqual(result, 105000.0, 45000.0)
+        result = Calculator(150000, 70, 4.5, 15,
+                            45100).calc_loan_mindeposit()
+        self.assertEqual(result, 105000.0/1, 45000.0/1)
 
 # Calculator(150000, 70, 4.5, 15, 45100)
 
