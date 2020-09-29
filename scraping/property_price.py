0a7b3691bd992972385cc523e70dc2b63a9d9a22
Winston <winston.barlow@code.berlin>
2020-09-29 13:16:06 +0200

    scrape property price

---
+++ scraping/property_price.py
@@ -0,0 +1,24 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = 'https://www.rightmove.co.uk/property-for-sale/property-84814774.html'
+
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.16 Safari/537.36'
+}
+
+# html_content = requests.get(url).text
+
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='primaryContent')
+
+price_element = results.find('p', class_='property-header-price')
+property_price = price_element.find('strong')
+property_location = results.find('address', class_="pad-0 fs-16 grid-25")
+
+
+print(property_price.text.strip())
+print(property_location.text.strip())
